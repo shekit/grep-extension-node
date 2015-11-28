0429dcf532dd386932bc9976038b4070b4128c00
shekit <as7790@nyu.edu>
2015-11-27 22:59:44 -0500

    added route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+router.post('/add', function(req,res,next){
+	res.send('added')
+})
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
